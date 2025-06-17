6ae45fc3a9b56646a8e2f69716cdca090b477bc1
Elias <eliassaulojoslimuyu@hotmail.com>
2025-06-16 21:18:46 -0500

    visual page home updated - only this

--- config/scramble.php
+++ config/scramble.php
@@ -39,7 +39,7 @@ return [
         /*
          * Define the title of the documentation's website. App name is used when this config is `null`.
          */
-        'title' => "Two Drive API Gigaprosita",
+        'title' => "Two Drive - Backend",
 
         /*
          * Define the theme of the documentation. Available options are `light` and `dark`.
