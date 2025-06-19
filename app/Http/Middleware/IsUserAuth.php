4b4fb6c4a1e4434c776782d1a9ea230fcf3648e5
Elias <eliassaulojoslimuyu@hotmail.com>
2025-06-19 11:42:34 -0500

    refactoring code 2

--- app/Http/Middleware/IsUserAuth.php
+++ app/Http/Middleware/IsUserAuth.php
@@ -18,7 +18,9 @@ class IsUserAuth
         if (auth('api')->user()) {
             return $next($request);
         } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 401);
         }
     }
 }
